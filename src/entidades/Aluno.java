cf8a17722a63a2b8674940a23eb4eae1f73c4133
Marcos santos <marcosvn3@academico.ufs.br>
2025-07-26 09:57:39 -0300

    Refatorando classe aluno

--- src/entidades/Aluno.java
+++ src/entidades/Aluno.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 
 public class Aluno extends Usuario{
 
+
     private Long matricula;
 
     public Aluno(String nome,String dataNascimento, String email,String senha,Long matricula) {
